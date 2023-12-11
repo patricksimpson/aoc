8f6ff5c69c335760b5bc0ccaee5c001991443d4b
Patrick Simpson <izerop@gmail.com>
2023-12-11 11:11:36 -0500

    day4 part-1

--- 2023/day4/day4.go
+++ 2023/day4/day4.go
@@ -9,8 +9,8 @@ import (
 )
 
 func main() {
-	readFile, err := os.Open("test.txt")
-	// readFile, err := os.Open("input.txt")
+	// readFile, err := os.Open("test.txt")
+	readFile, err := os.Open("input.txt")
 
 	if err != nil {
 		fmt.Println(err)
@@ -19,19 +19,39 @@ func main() {
   fileScanner := bufio.NewScanner(readFile)
   fileScanner.Split(bufio.ScanLines)
 	value := 0
-
+  wins := 0
+	game := 0
 	for fileScanner.Scan() {
 		str := fileScanner.Text()
 		l := strings.Split(str, ":")
 		gameLine := strings.Split(l[1], "|")
 		answers := strings.Split(gameLine[0], " ")
+		game += 1
+		wins = 0
 		for _, hand := range answers {
 			try := strings.TrimSpace(hand)
 			if (try != "") {
-				fmt.Println(try)
+				if(hasWinner(try, strings.Split(gameLine[1], " "))) {
+					if(wins > 0) {
+						wins = wins + wins
+					} else {
+						wins = 1
+					}
+				}
 			}
 		}
+		value += wins
+	}
+		
+	fmt.Println("value", value)
 	}
 
-	fmt.Println("value", value)
+func hasWinner(try string, line []string) bool {
+	for _, test := range line {
+		num := strings.TrimSpace(test)
+		if (strings.Compare(try, num) == 0) {
+			return true
+		}
+	}
+	return false
 }
